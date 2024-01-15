d0d28eabaece96ca2bf69f79280fda65553e7ac9
Jacobyou19 <Jacobyou19@gmail.com>
2024-01-15 11:38:38 +0800

    24115-MM

---
+++ pico_w/lesson7_2.py
@@ -0,0 +1,15 @@
+from machine import ADC, Pin
+from machine import Pin, Timer
+
+adc = ADC(4)  #create ADC object on ADC pin,最後一個, 溫度
+print(adc.read_u16())
+conversion_factor = 3.3/65535
+
+def second1(t):
+    reading_v = adc.read_u16() * conversion_factor
+    
+    celsius = 27 -(reading_v-0.706) / 0.001721
+    print(celsius)
+    
+tim1=Timer()
+tim1.init(period=1000, callback=second1)
