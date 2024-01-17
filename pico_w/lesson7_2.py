e2a0f81113918b18c8ea2951b7e1fa24b19fe0b9
Jacobyou19 <Jacobyou19@gmail.com>
2024-01-17 09:05:08 +0800

    24117-mm-1

--- pico_w/lesson7_2.py
+++ pico_w/lesson7_2.py
@@ -7,7 +7,8 @@ conversion_factor = 3.3/65535
 
 def second1(t):
     reading_v = adc.read_u16() * conversion_factor
-    
+    # The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
+    # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
     celsius = 27 -(reading_v-0.706) / 0.001721
     print(celsius)
     
