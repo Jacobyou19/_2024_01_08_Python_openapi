14e63130c4a4e72fcd1f2fde5f0237756433252f
Jacobyou19 <Jacobyou19@gmail.com>
2024-01-22 15:46:40 +0800

    AM-5

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -1,1 +1,13 @@
-from machine import pin
+from machine import Pin
+import time
+
+red_led = Pin(15,mode=Pin.OUT)
+btn = Pin(14,mode=Pin.PULL_DOWN)
+
+while True:
+    if btn.value():
+       red_led.value(1)
+    else:
+       red_led.value(0)
+       
+       time.sleep_ms(10)
