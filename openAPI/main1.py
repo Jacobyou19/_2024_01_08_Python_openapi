695d03950c313ccd1cd4866ab3df07a171b5c2d4
Jacobyou19 <Jacobyou19@gmail.com>
2024-01-22 11:17:50 +0800

    mm-2

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -18,7 +18,8 @@ async def get_item(item_id:int):
     print(f"使用者輸入了:(item_id)")
     return {"item_id": item_id}
 
-@app.get("/items/{item_id}")
-async def get_item(item_id:int):
-    print(f"日使用者輸入了:(item_id)")
-    return {"item_id": item_id}
+@app.get("/items/{date}/{celsius}")
+async def get_item(date:str,celsius:float):
+    print(f"日期:{date}")
+    print(f"溫度:{celsius}")
+    return {"日期":date,"攝氏溫度":celsius}
