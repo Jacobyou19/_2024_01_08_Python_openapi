1fc8b04893d913195e490b36cd32f242c421ab27
Jacobyou19 <Jacobyou19@gmail.com>
2024-01-24 14:47:43 +0800

    AM-1

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -43,7 +43,7 @@ while True:
                 
                 is_press = False
                 
-                url_str = f'https://https://openapli-test.onrender.com/pico_w/{currentTime}?address=chicken_KFC&celsius={temperature}&light={light}'
+                url_str = f'https://openapli-test.onrender.com/pico_w/{currentTime}?address=chicken_KFC&celsius={temperature}&light={light}'
                 try:
                     response = urequests.get(url_str)
                     pass
