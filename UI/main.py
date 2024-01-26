bfd2e2e7e8c91dcbbf890f2825bc03001f59bb78
Jacobyou19 <Jacobyou19@gmail.com>
2024-01-26 14:29:59 +0800

    modify

--- UI/main.py
+++ UI/main.py
@@ -3,7 +3,8 @@ import pandas as pd
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 
-url = 'https://openapi-test-6vnu.onrender.com/pico_w/?count=10'
+url = 'https://openapli-test.onrender.com/pico_w/?count=10'
+
 #自動reload頁面每10秒
 st_autorefresh(interval=10000, limit=100, key="fizzbuzzcounter")
 
